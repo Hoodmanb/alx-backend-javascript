5dd3a8a64b448fc5149a998323e12011932bf640
hoodmanb <joshuadebravo@gmail.com>
2024-07-24 23:16:05 +0100

    update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then((response) => ({ status: 200, body: 'success' }))
-    .catch((error) => Error())
-    .finally((final) => console.log('Got a response from the API'));
-}
+    .catch((error) => new Error())
+    .finally((info) => console.log('Got a response from the API'));
+};
