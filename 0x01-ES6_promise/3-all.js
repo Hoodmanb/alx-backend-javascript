ee0a3cd7451cdc7d55df66f16eca188f36ea639e
hoodmanb <joshuadebravo@gmail.com>
2024-07-25 15:46:57 +0100

    updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import {uploadPhoto, createUser} from './utils';
 
 export default function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
+    return Promise.all([uploadPhoto(), createUser()])
     .then(([resUploadPhoto, resCreateUser]) => {
         console.log(resUploadPhoto.body, resCreateUser.firstName, resCreateUser.lastName)
     })
