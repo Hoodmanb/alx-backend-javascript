03fedc9414c4b91a64d231028ee7be120e10ad62
hoodmanb <joshuadebravo@gmail.com>
2024-07-26 11:33:18 +0100

    1 update

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -4,7 +4,7 @@ export default function getFullResponseFromAPI(success) {
     if (success === true) {
       resolve({ status: 200, body: 'success' });
     } else {
-      reject(new Error('The fake API is not working currently'));
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
