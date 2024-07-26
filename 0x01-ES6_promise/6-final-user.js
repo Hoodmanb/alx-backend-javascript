8b0d8b03d2b3a100247ecea5b854c129d108ba0c
hoodmanb <joshuadebravo@gmail.com>
2024-07-26 21:09:59 +0100

    6 done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => {
+      response.map((obj) => ({
+        status: obj.status,
+        value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+      }));
+    });
+}
