bb4e3996897c52a9d315390803ed57a32185a8ef
hoodmanb <joshuadebravo@gmail.com>
2024-07-31 15:40:08 +0100

    push

---
+++ 0x04-TypeScript/task_0/main.ts
@@ -0,0 +1,18 @@
+interface Student {
+	firstName: string,
+	lastName: string,
+	age: number,
+	location: string
+}
+const student1: Student = {
+	firstName: 'john',
+	lastName: 'doe',
+	age: 16,
+	lacation: 'eutopia'
+}
+const student2: Student = {
+	firstName: 'mary',
+	lastName: 'jane',
+	age: 20,
+	lacation: 'destopia'
+}
