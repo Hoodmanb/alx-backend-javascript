f4fa774dac30838906023fc79d5d5166f0073bef
hoodmanb <joshuadebravo@gmail.com>
2024-07-31 01:01:59 +0100

    7, 8 added

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result += item.replace(startString, '') + '-';
+    }
+  }
+  if (result) {
+    result = result.slice(0, -1);
+  } if (!startString) {
+    result = ''
+  }
+  return result;
+}
