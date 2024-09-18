c5494b4407b224f06572d3d74757470f71ae3721
hoodmanb <joshuadebravo@gmail.com>
2024-09-18 18:00:28 +0100

    1 update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,7 +8,6 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
-  rl.close();
 });
 
 rl.on('SIGINT', () => {
