a5b3c5891c9f29a363a6a7808b75bf6d0ab5ecdc
hoodmanb <joshuadebravo@gmail.com>
2024-09-18 17:54:11 +0100

    0, 1 update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-const readline = require('node:readline');
+import readline from 'readline';
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question(`Welcome to Holberton School, what is your name?\n`, name => {
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+  rl.close();
 });
 
 rl.on('SIGINT', () => {
