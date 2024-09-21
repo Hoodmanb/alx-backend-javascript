31c16edcdbb51309c8f124aed07fd5702db6419f
hoodmanb <joshuadebravo@gmail.com>
2024-09-21 13:26:12 +0100

    3 updated

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,40 +6,37 @@ const countStudents = (path) => new Promise((resolve, reject) => {
     if (err) {
       reject(new Error('Cannot load the database'))
     }
-    const lines = data
-      .trim()
-      .split('\n')
-      .filter((line) => line.trim() !== '');
-
-    console.log(`Number of students: ${lines.length - 1}`);
-
-    const students = [];
-    const fields = [];
-    lines.forEach((line, index) => {
-      if (index !== 0) {
-        const [firstName, lastName, age, field] = line.split(',');
-        if (field) {
-          students.push({ firstName, lastName, age, field });
-          fields.push(field);
+    if (data) {
+      const lines = data
+        .trim()
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+      console.log(`Number of students: ${lines.length - 1}`);
+      const students = [];
+      const fields = [];
+      lines.forEach((line, index) => {
+        if (index !== 0) {
+          const [firstName, lastName, age, field] = line.split(',');
+          if (field) {
+            students.push({ firstName, lastName, age, field });
+            fields.push(field);
+          }
         }
-      }
-    });
-
-    const obj = {};
-    const noDuplicate = [...new Set(fields)];
-    noDuplicate.forEach((field) => {
-      obj[field] = [];
-    });
-
-    students.forEach((student) => {
-      obj[student.field].push(student.firstName);
-    });
-
-    Object.keys(obj).forEach((field) => {
-      const value = obj[field];
-      console.log(`Number of students in ${field}: ${value.length}. List: ${value.join(', ')}`);
-    });
-    resolve()
+      });
+      const obj = {};
+      const noDuplicate = [...new Set(fields)];
+      noDuplicate.forEach((field) => {
+        obj[field] = [];
+      });
+      students.forEach((student) => {
+        obj[student.field].push(student.firstName);
+      });
+      Object.keys(obj).forEach((field) => {
+        const value = obj[field];
+        console.log(`Number of students in ${field}: ${value.length}. List: ${value.join(', ')}`);
+      });
+      resolve()
+    }
   })
 })
 
