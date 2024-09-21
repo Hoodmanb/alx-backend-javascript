6047c94b4815a252719c977ca25ba93fd776c67a
hoodmanb <joshuadebravo@gmail.com>
2024-09-21 15:35:15 +0100

    1, 5, 6 updated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
 /* eslint-disable */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.once('data', (data) => {
-  // const name = data.toString().trim();
-  console.log(`Your name is: ${data}`);
-  process.stdin.destroy();
-});
-
-process.on('SIGINT', () => {
-  console.log('This important software is now closing');
-  process.exit();
+  process.stdout.write(`Your name is: ${name}\n`);
+  if (process.stdin.isTTY) {
+    process.on('SIGINT', () => {
+      console.log('This important software is now closing');
+    });
+  } 
+  process.exit()
 });
